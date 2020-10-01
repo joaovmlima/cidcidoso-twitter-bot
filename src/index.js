52e7b0f695fef5940697c559e7b93e21ab363140
João Victor <oi.oaoj@gmail.com>
2020-10-01 11:35:34 -0300

    improve logic and prevent multiple replies

--- src/index.js
+++ src/index.js
@@ -7,30 +7,37 @@ const app = express()
 
 app.use(express.json())
 
-let screen_name = 'naosalvo' //Twitter @ (username) of the account you want to reply to
-let lastId = 0
-
-app.post('/trigger', async (req, res) => {
-  try {
-    const { secret } = req.body
-
-    if (secret != process.env.ACCESS_TOKEN_SECRET) return res.status(401).end()
-
-    let id = await twitter.getLastTweetFrom(`${screen_name}`)
-
-    if (lastId === id) return res.status(406).send("Tweet already replied").end()
-
-    await twitter.tweetMessage(`@${screen_name} ${getRandomMessage()}`, id)
-    lastId = id
-    res.status(204).end()
-
-  } catch (error) {
-    res.status(500).send(error.message)
-  }
-})
-
 const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`)
 })
+
+let screen_name = 'CidBacanoso' //Twitter @ (username) of the account you want to reply to
+
+app.post('/trigger', async (req, res) => {
+  try {
+    const { secret } = req.body
+    //denies the request if the token isn't right
+    if (secret != process.env.ACCESS_TOKEN_SECRET) return res.status(401).end()
+
+    let id = await twitter.getLastTweetFrom(screen_name)
+
+    //searches in the DB if the tweet has already been replied
+    let foundTweet = await twitter.searchHistory(id)
+
+    if (foundTweet === 0) {
+      await twitter.tweetMessage(`@${screen_name} ${getRandomMessage()}`, id)
+      twitter.addToHistory(id)
+      res.status(204).end()
+    }
+    else {
+      res.status(406).send('tweet already replied!')
+    }
+
+  }
+  catch (error) {
+    res.status(500).send(error.message)
+  }
+})
+
