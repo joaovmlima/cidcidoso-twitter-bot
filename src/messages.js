f071fb7101d151d98743922d82f2662754eeb2d6
João Victor <oi.oaoj@gmail.com>
2020-10-08 08:38:04 -0300

    add new messages

--- src/messages.js
+++ src/messages.js
@@ -11,9 +11,19 @@ const MESSAGES = [
   'Cid como faz pra ter uma barba de macho igual a sua?',
   'Cid NUNCA deu calote',
   'B? H? G?',
-  'Brabo\nHumilde\God',
+  'Brabo\nHumilde\nGod',
   'Cid pai de todos nós',
-  'Cid é fundador da internet brasileira'
+  'Cid é fundador da internet brasileira',
+  'Você é mais que 10, é 9?',
+  'Meu amor por você, não é Catfish',
+  'Você é tão legal quanto o sorteio de um Monza 97!',
+  'timboto?!',
+  'Você não está lindo hoje, está faustop!',
+  'seu cidade você vai viajar? que pacotão é esse ai?',
+  'se você é feliz na internet hoje agradeça ao cid',
+  'feliz aniversário cid',
+  'cid é onipresente',
+  'cid se você nunca dorme, é sempre bom dia?\n\nbom dia'
 
 ]
 
